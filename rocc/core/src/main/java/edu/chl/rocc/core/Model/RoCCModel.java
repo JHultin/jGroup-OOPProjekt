020dc3c074a9f3cb54e2165b087c90684fd05b20
BeardedRoar <joeljoel224@hotmail.com>
2015-04-22 15:04:26 +0200

    made functions for moving in model

--- rocc/core/src/main/java/edu/chl/rocc/core/Model/RoCCModel.java
+++ rocc/core/src/main/java/edu/chl/rocc/core/Model/RoCCModel.java
@@ -1,5 +1,7 @@
 package edu.chl.rocc.core.Model;
 
+import com.badlogic.gdx.Input;
+
 /**
  * Created by Yen on 2015-04-22.
  */
@@ -9,19 +11,19 @@ public class RoCCModel {
         //Level level= new Level();
     }
 
+    public void keyPressed(int keyCode){
+        if (keyCode == Input.Keys.RIGHT){
+            this.moveSideways();
+        } else if (keyCode == Input.Keys.LEFT){
+            this.moveSideways();
+        }
+    }
 
+    public void keyReleased(int keyCode){
 
+    }
 
+    private void moveSideways(){
 
-
-
-
-
-
-
-
-
-
-
-
+    }
 }
