819c656c6a7efea417e8b923f94a08858cf48e9f
Jacob <jacobduvander@gmail.com>
2015-04-22 14:40:31 +0200

    Updated RoCCView

--- rocc/core/src/main/java/edu/chl/rocc/core/RoCC.java
+++ rocc/core/src/main/java/edu/chl/rocc/core/RoCC.java
@@ -1,21 +1,28 @@
 package edu.chl.rocc.core;
 
 import com.badlogic.gdx.graphics.GL30;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 
 public class RoCC implements ApplicationListener {
-	Texture texture;
-	SpriteBatch batch;
-	float elapsed;
+	private Texture texture;
+	private SpriteBatch batch;
+
+	private float elapsed;
 
 	@Override
 	public void create () {
 		texture = new Texture(Gdx.files.internal("libgdx-logo.png"));
 		batch = new SpriteBatch();
-	}
+
+    }
 
 	@Override
 	public void resize (int width, int height) {
--- rocc/core/src/main/java/edu/chl/rocc/core/view/RoCCView.java
+++ rocc/core/src/main/java/edu/chl/rocc/core/view/RoCCView.java
@@ -4,6 +4,7 @@ import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.graphics.glutils.ShaderProgram;
 import com.badlogic.gdx.math.Matrix4;
@@ -11,18 +12,23 @@ import com.badlogic.gdx.math.Matrix4;
 import java.util.List;
 
 /**
+ * This class updates the view.
+ *
  * Created by Jacob on 2015-04-22.
  */
 public class RoCCView{
 
-    private Batch batch;
+    private RoCCModel model;
 
-    public RoCCView(){
+
+    public RoCCView(RoCCModel model){
+        this.model = model;
 
     }
 
-    public void draw(List<Sprite> spriteList){
-        for(Sprite sprite : spriteList){
+    public void draw(SpriteBatch batch){
+
+        for(Sprite sprite : model.getSprites()){
             batch.draw(sprite, sprite.getX(), sprite.getY());
         }
     }
