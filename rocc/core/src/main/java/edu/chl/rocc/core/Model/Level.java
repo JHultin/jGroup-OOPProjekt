27cb033df855e58f4566fcb80a2c0c63a266e3d0
YenL <jennifer.linder@live.com>
2015-04-22 15:14:39 +0200

    Levels move method done

--- rocc/core/src/main/java/edu/chl/rocc/core/Model/Level.java
+++ rocc/core/src/main/java/edu/chl/rocc/core/Model/Level.java
@@ -1,13 +1,21 @@
 package edu.chl.rocc.core.Model;
 
+import java.util.ArrayList;
+
 /**
  * Created by Yen on 2015-04-22.
  */
 public class Level {
+    private int time;
+    private int score;
+    private ArrayList <String> highscore;
 
-    public Level() {
+    public Level(){
 
+    }
 
-
+    public void move(){
+        Character character = new Character();
+        character.move();
     }
 }
