88a28c269f0fefe3693d1e790bdcd06bad163b92
Dreamium <orell@student.chalmers.se>
2015-04-22 14:51:49 +0200

    Added comments and made other minor changes to class 'Character'

--- rocc/core/src/main/java/edu/chl/rocc/core/Model/Character.java
+++ rocc/core/src/main/java/edu/chl/rocc/core/Model/Character.java
@@ -1,7 +1,10 @@
-package edu.chl.rocc.core;
+package edu.chl.rocc.core.Model;
 
 /**
- * Created by Jenny on 2015-04-22.
+ * A class for the playable characters.
+ * Handeling data and setters/getters.
+ *
+ * @author Jenny Orell
  */
 public class Character {
 
@@ -12,22 +15,39 @@ public class Character {
         this.setHP(maxHealth);
     }
 
+    /*
+    * Returns the character's health.
+    */
     public int getHP(){
         return this.healthPoints;
     }
 
+    /*
+    * Set character's health with a chosen value.
+    */
     public void setHP(int value){
-        if(value <= maxHealth){
+        if(value < 0){
+            System.out.print("Input value for health points cannot be negative.");
+            // die-method??
+        }
+
+        if(value <= maxHealth && value >= 0){
             this.healthPoints = value;
-        } else{
+        }else{
             this.healthPoints = maxHealth;
         }
     }
 
+    /*
+    * Increase character's health with a chosen value.
+    */
     public void incHP(int value){
         this.setHP(this.getHP() + value);
     }
 
+    /*
+    * Decrease character's health with a chosen value.
+    */
     public void decHP(int value){
         this.setHP(this.getHP() - value);
     }
