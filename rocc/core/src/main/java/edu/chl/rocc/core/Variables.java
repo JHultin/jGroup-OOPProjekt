ab4853462a3098b2b1d64c76dcf9f1507eb3d2d3
Jacob <jacobduvander@gmail.com>
2015-04-27 23:27:43 +0200

    added a class for global Variables and fixed so that camera follows

---
+++ rocc/core/src/main/java/edu/chl/rocc/core/Variables.java
@@ -0,0 +1,19 @@
+package edu.chl.rocc.core;
+
+/**
+ * A class containing global variables
+ * for easier access.
+ * Created by Jacob on 2015-04-27.
+ */
+public class Variables {
+
+    //Pixels per meter ratio
+    public static final float PPM = 100;
+
+    //The gravity in standard newton
+    public static final double GRAVITY = -9.81;
+
+    //Width and Height of the camera
+    public static final int WIDTH = 320;
+    public static final int HEIGHT = 240;
+}
