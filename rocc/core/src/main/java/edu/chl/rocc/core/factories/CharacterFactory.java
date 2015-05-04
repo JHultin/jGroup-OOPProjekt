9db3b41803ab98e2cdee8d6894325cfd08edc232
BeardedRoar <joeljoel224@hotmail.com>
2015-05-04 20:38:43 +0200

    created ICharacterFactory

---
+++ rocc/core/src/main/java/edu/chl/rocc/core/factories/CharacterFactory.java
@@ -0,0 +1,14 @@
+package edu.chl.rocc.core.factories;
+
+import edu.chl.rocc.core.m2phyInterfaces.ICharacter;
+
+/**
+ * @author Jenny Orell
+ */
+public class CharacterFactory implements ICharacterFactory {
+
+    @Override
+    public ICharacter createCharacter(String name) {
+        return null;
+    }
+}
