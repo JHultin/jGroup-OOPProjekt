3270525fd166a7d98032242bff58bb8d6dac48db
Jacob <jacobduvander@gmail.com>
2015-05-05 16:36:53 +0200

    added a class for view variables

---
+++ rocc/core/src/main/java/edu/chl/rocc/core/view/ViewVariables.java
@@ -0,0 +1,13 @@
+package edu.chl.rocc.core.view;
+
+/**
+ * A class to
+ * Created by Jacob on 2015-05-05.
+ */
+public class ViewVariables {
+
+    //The numbers doesn't meen anything special
+    protected static final int PLAY = 919123;
+    protected static final int MENU = 872122;
+
+}
