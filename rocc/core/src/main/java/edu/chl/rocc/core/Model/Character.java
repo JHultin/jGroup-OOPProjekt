e62f4f6db3bd682b455c374cdf8c847bb3ed183f
YenL <jennifer.linder@live.com>
2015-05-05 16:32:42 +0200

    refactor physic character

--- rocc/core/src/main/java/edu/chl/rocc/core/Model/Character.java
+++ rocc/core/src/main/java/edu/chl/rocc/core/Model/Character.java
@@ -21,45 +21,11 @@ import org.jbox2d.dynamics.World;
  */
 public class Character implements ICharacter {
 
-    private final World world;
     private int maxHealth = 100;
     private int healthPoints;
-    private Body body;
-    private int width, height;
-    private int BIT_Body;
-    private int BIT_Ground;
 
-    public Character(World world){
+    public Character(){
         this.setHP(maxHealth);
-        this.world = world;
-        this.width = 18;
-        this.height = 35;
-        this.BIT_Body = 4;
-        this.BIT_Ground = 2;
-        this.world.setContactListener(new MyContactListener()); //Ska flyttas
-
-        //Defining & creating body
-        BodyDef def = new BodyDef();
-        def.position.set(160,120);
-        def.type = BodyType.DYNAMIC;
-        body = this.world.createBody(def);
-
-        //Defining & creating fixture
-        PolygonShape shape = new PolygonShape();
-        shape.setAsBox(width,height);
-        FixtureDef fDef = new FixtureDef();
-        fDef.shape = shape;
-        fDef.filter.categoryBits = BIT_Body;
-        fDef.filter.maskBits = BIT_Ground;
-        body.createFixture(fDef).setUserData("playerBody");
-
-        //create foot sensor
-        shape.setAsBox(width,height/4,new Vec2(0,-30),0);
-        fDef.shape = shape;
-        fDef.filter.categoryBits = BIT_Body;
-        fDef.filter.maskBits = BIT_Ground;
-        fDef.isSensor = true;
-        body.createFixture(fDef).setUserData("footSensor");
     }
 
     /*
@@ -103,38 +69,23 @@ public class Character implements ICharacter {
     * Move the character in a given direction.
     */
     public void move(Direction dir){
-        if(dir.equals(Direction.LEFT)){
-            body.applyForceToCenter(new Vec2(-1000, 0));
-        } else if(dir.equals(Direction.RIGHT)){
-            //xPos += 15;
-            body.applyForceToCenter(new Vec2(200, 0));
-        } else if(dir.equals(Direction.UP)){
-            //yPos += 15;
-            body.applyForceToCenter(new Vec2(200, 0));
-        } else if(dir.equals(Direction.DOWN)){
-
-        } else if (dir.equals(Direction.NONE)){
-
-        }
-
     }
 
     public void jump(){
-        body.applyForceToCenter(new Vec2(0, 1000));
     }
 
     /*
     * Returns the x-coordinate of the character.
     */
     public float getX(){
-        return body.getPosition().x-width;
+        return 0;
     }
 
     /*
     * Returns the y-coordinate of the character.
     */
     public float getY(){
-        return body.getPosition().y-height;
+        return 0;
     }
 
 }
