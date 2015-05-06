2f20e319da88d444ed302a082987219478f55807
BeardedRoar <joeljoel224@hotmail.com>
2015-05-06 12:11:54 +0200

    moved constructWorld to PhyModel

--- rocc/core/src/main/java/edu/chl/rocc/core/Model/RoCCModel.java
+++ rocc/core/src/main/java/edu/chl/rocc/core/Model/RoCCModel.java
@@ -31,43 +31,7 @@ public class RoCCModel implements IRoCCModel {
 
     // Creates a logical box2d map mimicing the tiled-map
     public void constructWorld(TiledMap tMap){
-        // Get the layer with information about the solid ground
-        TiledMapTileLayer tileLayer = (TiledMapTileLayer)tMap.getLayers().get("ground");
 
-        // Create a tile for each block on the map
-        for (int row = 0; row < tileLayer.getHeight(); row++){
-            for (int col = 0; col < tileLayer.getWidth(); col++){
-                TiledMapTileLayer.Cell cell = tileLayer.getCell(col, row);
-
-                // If there is a tile at the position
-                if (cell != null && cell.getTile() != null){
-
-                    // Create a body definiion
-                    BodyDef bDef = new BodyDef();
-                    bDef.type = BodyType.STATIC;
-                    bDef.position.set(32 * (col + 0.5f), 32 * (row + 0.5f));
-
-                    // And a fixture definition
-                    ChainShape cs = new ChainShape();
-                    Vec2[] v = new Vec2[5];
-                    v[0] = new Vec2( -16f, -16f);
-                    v[1] = new Vec2( -16f,  16f);
-                    v[2] = new Vec2(  16f,  16f);
-                    v[3] = new Vec2(  16f, -16f);
-                    v[4] = new Vec2( -16f, -16f);
-                    cs.createChain(v, 5);
-
-                    FixtureDef fDef = new FixtureDef();
-                    fDef.friction = 0;
-                    fDef.shape = cs;
-                    fDef.filter.categoryBits = 2;  // As Character variable BIT_Ground
-                    fDef.filter.maskBits = 4;      // As Character variable BIT_Body
-
-                    // Then let the level create the block in the world
-                    level.addBlock(bDef, fDef);
-                }
-            }
-        }
     }
 
     public void moveSideways(Direction dir){
