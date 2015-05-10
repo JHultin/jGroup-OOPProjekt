b161af096df9b6886f5e61ba5748d37305cfe1e3
Jacob <jacobduvander@gmail.com>
2015-05-10 14:24:55 +0200

    changed so that everything uses Screen

--- rocc/core/src/main/java/edu/chl/rocc/core/view/GameViewManager.java
+++ rocc/core/src/main/java/edu/chl/rocc/core/view/GameViewManager.java
@@ -1,13 +1,11 @@
 package edu.chl.rocc.core.view;
 
 import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import edu.chl.rocc.core.RoCCView;
 import edu.chl.rocc.core.m2phyInterfaces.IRoCCModel;
-import edu.chl.rocc.core.model.MenuModel;
-import edu.chl.rocc.core.model.RoCCModel;
-
 import java.util.HashMap;
 import java.util.Stack;
 
@@ -18,12 +16,16 @@ import java.util.Stack;
  */
 public class GameViewManager {
 
-    private HashMap<String, GameView> viewHashMap;
-    private GameView activeView;
+    private HashMap<String, Screen> viewHashMap;
+    private Screen activeView;
 
     public GameViewManager(IModel menuModel, IRoCCModel roccModel){
-        viewHashMap = new HashMap<String, GameView>();
-        //Adds views to HashMap
+        viewHashMap = new HashMap<String,Screen>();
+
+        /**
+         *  Adds views to a HashMap so that they don't need
+         *  to be creating new everytime.
+         */
         viewHashMap.put("MENU", ViewFactory.createView("MENU", menuModel));
         viewHashMap.put("PLAY",ViewFactory.createView("PLAY", roccModel));
 
@@ -32,12 +34,12 @@ public class GameViewManager {
      }
 
 
-    public void update(){
-        activeView.update();
+    public void setActiveView(String view){
+        activeView = viewHashMap.get(view);
     }
 
-    public void render(SpriteBatch batch, OrthographicCamera cam, OrthographicCamera hudCam){
-        activeView.render(batch, cam,hudCam);
+    public Screen getActiveView(){
+        return activeView;
     }
 
 }
