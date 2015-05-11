5ef91535db442f9beb3ea7201b4ad0ca9fcff47c
BeardedRoar <joeljoel224@hotmail.com>
2015-05-11 16:40:12 +0200

    created inteface for all pickupable things

--- rocc/core/src/main/java/edu/chl/rocc/core/m2phyInterfaces/IFood.java
+++ rocc/core/src/main/java/edu/chl/rocc/core/m2phyInterfaces/IFood.java
@@ -3,9 +3,6 @@ package edu.chl.rocc.core.m2phyInterfaces;
 /**
  * Created by Joel on 2015-05-08.
  */
-public interface IFood {
+public interface IFood extends IPlayer{
 
-    public float getX();
-
-    public float getY();
 }
