71aceaf0f02b2f9eb68349359a4d0311dd5242cc
YenL <jennifer.linder@live.com>
2015-05-11 16:02:32 +0200

    bitmask for bullet added

--- rocc/core/src/main/java/edu/chl/rocc/core/physics/PhyRoCCModel.java
+++ rocc/core/src/main/java/edu/chl/rocc/core/physics/PhyRoCCModel.java
@@ -83,7 +83,7 @@ public class PhyRoCCModel implements IRoCCModel {
         fDef.friction = 0;
         fDef.shape = cs;
         fDef.filter.categoryBits = BitMask.BIT_GROUND;
-        fDef.filter.maskBits = BitMask.BIT_BODY | BitMask.BIT_ENEMY;
+        fDef.filter.maskBits = BitMask.BIT_BODY | BitMask.BIT_ENEMY | BitMask.BIT_BULLET;
 
         // Create a tile for each block on the map
         for (int row = 0; row < tileLayer.getHeight(); row++) {
