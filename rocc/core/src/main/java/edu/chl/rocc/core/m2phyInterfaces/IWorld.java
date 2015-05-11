9d2e55ec4ddbcb475033f961e4533c7b6ca826b1
YenL <jennifer.linder@live.com>
2015-05-11 17:12:30 +0200

    created IWorld and PhyWorld

---
+++ rocc/core/src/main/java/edu/chl/rocc/core/m2phyInterfaces/IWorld.java
@@ -0,0 +1,7 @@
+package edu.chl.rocc.core.m2phyInterfaces;
+
+/**
+ * Created by Yen on 2015-05-11.
+ */
+public interface IWorld {
+}
---
+++ rocc/core/src/main/java/edu/chl/rocc/core/physics/PhyWorld.java
@@ -0,0 +1,9 @@
+package edu.chl.rocc.core.physics;
+
+/**
+ * Created by Yen on 2015-05-11.
+ */
+public class PhyWorld {
+
+
+}
