218fe2171c33af0c81a96e3ee7e9be88eb362a96
BeardedRoar <joeljoel224@hotmail.com>
2015-05-12 21:42:12 +0200

    added comments to factories

--- rocc/core/src/main/java/edu/chl/rocc/core/factories/IRoCCFactory.java
+++ rocc/core/src/main/java/edu/chl/rocc/core/factories/IRoCCFactory.java
@@ -3,6 +3,8 @@ package edu.chl.rocc.core.factories;
 import edu.chl.rocc.core.m2phyInterfaces.*;
 
 /**
+ * Iterface for factories creating all objects handling the logic in our game
+ *
  * Created by Joel on 2015-05-08.
  */
 public interface IRoCCFactory {
