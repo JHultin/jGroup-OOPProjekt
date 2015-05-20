9e0222edacca2cff06ad55e6c4c32af91cfa42d1
BeardedRoar <joeljoel224@hotmail.com>
2015-05-20 16:01:05 +0200

    created dummyTestFile

---
+++ rocc/core/src/test/java/edu/chl/rocc/core/model/CharacterTest.java
@@ -0,0 +1,76 @@
+package edu.chl.rocc.core.model;
+
+import static org.junit.Assert.*;
+
+public class CharacterTest {
+
+    @org.junit.Test
+    public void testGetHP() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testSetHP() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testIncHP() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testDecHP() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testMove() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testIsMoving() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testJump() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testJumpIfFollower() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testToggleFollowerOnJumpPoint() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testIsOnJumpPoint() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testHitGround() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testLeftGround() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testGetX() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testGetY() throws Exception {
+
+    }
+}
