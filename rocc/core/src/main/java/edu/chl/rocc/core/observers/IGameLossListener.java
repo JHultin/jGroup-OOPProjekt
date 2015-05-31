4f74bad3e0b540f39926c3b453b907e07dccef05
BeardedRoar <joeljoel224@hotmail.com>
2015-05-31 23:21:01 +0200

    commented observers

--- rocc/core/src/main/java/edu/chl/rocc/core/observers/IGameLossListener.java
+++ rocc/core/src/main/java/edu/chl/rocc/core/observers/IGameLossListener.java
@@ -1,9 +1,13 @@
 package edu.chl.rocc.core.observers;
 
 /**
+ * Interface for listeners of games that can lose
  * Created by Joel on 2015-05-28.
  */
 public interface IGameLossListener {
 
+    /**
+     * Triggered when a game that is listened to is lost
+     */
     public void gameLost();
 }
