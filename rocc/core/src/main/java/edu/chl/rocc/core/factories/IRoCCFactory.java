7294e6083de30c15dcd15955cd2a6e04aa8445da
Dreamium <orell@student.chalmers.se>
2015-05-31 15:38:23 +0200

    Added javadoc for IRoCCFactory

--- rocc/core/src/main/java/edu/chl/rocc/core/factories/IRoCCFactory.java
+++ rocc/core/src/main/java/edu/chl/rocc/core/factories/IRoCCFactory.java
@@ -9,18 +9,46 @@ import edu.chl.rocc.core.m2phyInterfaces.*;
  */
 public interface IRoCCFactory {
 
+    /**
+     * Creates a player with a given ID/name.
+     * @return the created player.
+     */
     public IPlayer createPlayer(String name);
 
+    /**
+     * Creates a level with a given ID/name.
+     * @return the created level.
+     */
     public ILevel createLevel (String name);
 
+    /**
+     * Creates a character with a given ID/name at a specific location.
+     * @return the created character.
+     */
     public ICharacter createCharacter(String name, float x, float y);
 
+    /**
+     * Creates a food object with a given ID/name at a specific location.
+     * @return the created food.
+     */
     public IFood createFood(String name, int x, int y);
 
+    /**
+     * Creates a weapon with a given ID/name at a specific location.
+     * @return the created weapon.
+     */
     public IWeapon createWeapon(String name, float x, float y);
 
+    /**
+     * Creates a character that the player can pick up, with a given ID/name at a specific location.
+     * @return the created pickupable character.
+     */
     public IPickupableCharacter createPickupAbleCharacter(String name, int x, int y);
 
+    /**
+     * Creates an enemy with a given ID/name at a specific location.
+     * @return the created enemy.
+     */
     public IEnemy createEnemy(String name, int x, int y, int hp);
 
 }
